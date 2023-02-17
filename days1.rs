203ad2ce5130c730fdcb2e0fa3763744b6378f4f
sagarregmi2056 <115308062+sagarregmi2056@users.noreply.github.com>
2023-02-17 21:00:11 +0545

    Add files via upload

---
+++ days1.rs
@@ -0,0 +1,21 @@
+
+// datatypein rust 
+
+
+fn
+main(){
+   let fees = 2000; 
+   //the fees is here as integer
+
+   let name = "sagar";
+   // the name is here as character 
+
+   let attendence:bool = true;
+   //here we have given the bool type for attendence 
+
+   println!("fees is{} and name is{} and attendence is {}",fees,name,attendence);
+   //here the {} is a placeholder for the parameters of the given variables
+//    Immutable
+// By default, variables are immutable − read only in Rust.
+
+}
